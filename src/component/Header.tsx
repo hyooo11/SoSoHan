2c11aa6d9235f29b1a4b3351f3a2ebda9f11216b
hyooo11 <tlsgywls1107@naver.com>
2024-03-13 23:42:15 +0900

    style: InputFormIconLabel input 간격조정

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -3,14 +3,17 @@
 import NavMenu from "@/component/NavMenu";
 import PostSearch from "@/component/PostSearch";
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 import { IoSearch } from "react-icons/io5";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoClose } from "react-icons/io5";
 
 const Header = () => {
+  const pathname = usePathname();
   const [searchToggle, setSearchToggle] = useState(false);
   const [navToggle, setnNavToggle] = useState(false);
 
+  if (pathname.includes("/auth/login")) return null;
   return (
     <>
       <div className="Header">
