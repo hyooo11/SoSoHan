9ac591fdb80b9b99278e27038a02eb3d2807e611
hyooo11 <tlsgywls1107@naver.com>
2024-04-15 05:15:07 +0900

    fix: 모바일 input 확대 방지

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import RecoilRootProvider from "@/recoil/recoilRootProvider";
@@ -6,7 +7,12 @@ import LoginCheck from "@/util/LoginCheck";
 import SetScreenHeight from "@/util/SetScreenHeight";
 
 const inter = Inter({ subsets: ["latin"] });
-
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
 export const metadata: Metadata = {
   title: "소소한",
   description: "소소한 일상을 기록하고 공유하는 블로그",
